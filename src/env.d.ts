683084a28e0237b01adf8d00fa9decaccb72b512
BYSu <mayday15221@gmail.com>
2021-09-19 10:48:24 +0800

    add more env config

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,8 @@
 interface ImportMetaEnv {
     readonly VITE_APP_TITLE: string
     readonly VITE_IS_LOCAL: boolean
+    readonly VITE_BACKEND_HOST: string
+    readonly VITE_APP_VERSION: string
 }
 
 interface ImportMeta {
